19a692e12f3c4f81d538f51ec2c008df202b2a67
Scott Werner <stwerner@vt.edu>
2023-12-08 14:35:33 -0500

    Update agent generator for latest format

--- lib/sublayer/agents/generate_sublayer_agent_agent.rb
+++ lib/sublayer/agents/generate_sublayer_agent_agent.rb
@@ -27,7 +27,7 @@ module Sublayer
         Or a Sublayer Agent can perform a particular task like this: #{File.read(File.expand_path(__dir__ + "/save_file_contents_agent.rb"))}
 
         A Sublayer agent has an execute method, and initialize method that takes input, a result function that describes the expected output from the LLM (if it is using LLM assistance) and a prompt that may or may not use the input.
-        The LLMAssistance module has a generate_to_single_function_call method that takes the prompt and result function and sends it to an LLM for processing
+        The LLMAssistance module adds llm_generate which uses the prompt and llm_result_format and sends it to an LLM for processing
         The HumanAssistance module presents the string to a human for verification and modification.
 
         You need to build a new Sublayer agent that does the following:
