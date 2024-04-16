132984b69fd9e12962796bc244a97670f5cc13f7
Dr Nic Williams <drnicwilliams@gmail.com>
2024-04-16 15:04:33 +1000

    Allow OutputAdapters to be from any namespace

--- spec/components/output_adapter_spec.rb
+++ spec/components/output_adapter_spec.rb
@@ -41,4 +41,24 @@ RSpec.describe Sublayer::Components::OutputAdapters do
       end
     end
   end
+
+  context "class:" do
+    it "class: 'String'" do
+      output_adapter = Sublayer::Components::OutputAdapters.create(
+        class: "Sublayer::Components::OutputAdapters::SingleString",
+        name: "modified_file_contents",
+        description: "The modified file contents"
+      )
+      expect(output_adapter).to be_a(Sublayer::Components::OutputAdapters::SingleString)
+    end
+
+    it "class: Constant" do
+      output_adapter = Sublayer::Components::OutputAdapters.create(
+        class: Sublayer::Components::OutputAdapters::SingleString,
+        name: "modified_file_contents",
+        description: "The modified file contents"
+      )
+      expect(output_adapter).to be_a(Sublayer::Components::OutputAdapters::SingleString)
+    end
+  end
 end
