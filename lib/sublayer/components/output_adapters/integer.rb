86383de95e3cb24f6def03c3f3011fca795eebb7
Andrew Kang <andrew.b.kang@gmail.com>
2024-05-02 09:43:17 -0700

    add integer output adapter

---
+++ lib/sublayer/components/output_adapters/integer.rb
@@ -0,0 +1,18 @@
+module Sublayer
+  module Components
+    module OutputAdapters
+      class Integer
+        attr_reader :name, :description
+
+        def initialize(options)
+          @name = options[:name]
+          @description = options[:description]
+        end
+
+        def properties
+          [OpenStruct.new(name: @name, type: 'integer', description: @description, required: true)]
+        end
+      end
+    end
+  end
+end
